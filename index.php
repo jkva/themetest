ea5a5041192bfc74905905e1fbe42a99810a167e
jkva <mail@3ode.nl>
2016-08-31 11:01:31 +0200

    Skeleton

---
+++ index.php
@@ -0,0 +1,15 @@
+/*
+Theme Name: Tau Station
+Theme URI: N/A
+Author: Tau Station Team
+Author URI: http://www.taustation.space
+Description: The Tau Station website theme
+Version: 0.1
+License: GNU General Public License v2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+Tags: dark
+Text Domain: taustationspace
+
+This theme, like WordPress, is licensed under the GPL.
+Use it to make something cool, have fun, and share what you've learned with others.
+*/
